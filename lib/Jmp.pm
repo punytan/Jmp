5eb80f3cccc9b85ff2773a8f2e56773824ec35e7
punytan <punytan@gmail.com>
2012-04-07 00:32:37 +0900

    initial commit

---
+++ lib/Jmp.pm
@@ -0,0 +1,32 @@
+package Jmp;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Jmp -
+
+=head1 SYNOPSIS
+
+  use Jmp;
+
+=head1 DESCRIPTION
+
+Jmp is
+
+=head1 AUTHOR
+
+punytan E<lt>punytan@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
