1631564d286713575bf81954847e752bb3de1b86
punytan <punytan@gmail.com>
2012-04-12 01:27:59 +0900

    alter router usage

--- app.psgi
+++ app.psgi
@@ -1,12 +1,12 @@
 use sane;
-use FindBin::libs;
 use Jmp::Web;
+use Jmp::Router;
 use Jmp::DB::Session;
 
 my $session = Jmp::DB::Session->new;
 
 my $jmp = Jmp::Web->new(
-    handler_class => "Jmp::Web",
+    router => Jmp::Router->register,
     middlewares => {
         Session => {
             Store => { DBI => { get_dbh => sub { $session->dbh } } }
--- lib/Jmp/Router.pm
+++ lib/Jmp/Router.pm
@@ -2,12 +2,16 @@ package Jmp::Router;
 use sane;
 use Router::Lazy;
 
-my $router = Router::Lazy->instance("Jmp::Web");
+sub register {
+    my $router = Router::Lazy->instance("Jmp::Web");
 
-$router->get("/" => "Root#index");
-$router->get("/shorten" => "Root#index");
-$router->post("/shorten" => "Shorten#shorten");
-$router->get("/id/:id"  => "Redirect#redirector");
+    $router->get("/" => "Root#index");
+    $router->get("/shorten" => "Root#index");
+    $router->post("/shorten" => "Shorten#shorten");
+    $router->get("/id/:id"  => "Redirect#redirector");
+
+    return $router;
+}
 
 1;
 __END__
